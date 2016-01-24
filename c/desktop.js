54e8fd4b2ff45932fe0af385832677f268061e3a
vagrant <vagrant@precise64.(none)>
2016-01-24 23:12:58 +0000

    first commit

---
+++ c/desktop.js
@@ -0,0 +1,70 @@
+define([
+{
+   title: "Windows Screensaver Plus"
+  ,ico:"computer"
+  ,exe:"computer"
+  ,exe:"hello"
+  ,help:"Type help for help"
+}
+,'<br>',
+{
+   title: "C:/"
+  ,ico:"hd"
+  ,url:"c/"
+}
+,'<br>',
+{
+   title: "Terminal"
+  ,ico:"terminal"
+  ,exe:"terminal"
+},
+{
+   title: "Screensaver++"
+  ,ico:"magic"
+  ,exe:"screensaverpp"
+},{
+   title: "Virtual Screensaver"
+  ,ico:"inception"
+  ,url: document.URL
+  ,width:"600"
+  ,height:"400"
+  ,help:"INCEPTION"
+},
+{
+   title: "3D Text"
+  ,ico:"question"
+  ,exe:"3d"
+}
+,'<br>',
+{
+   title: "Notes"
+  ,exe:"note"
+  ,ico:"pin"
+},
+'<br>',
+{
+   title: "HAMPSTER"
+  ,ico:"/c/programs/demos/hampsterDance/icon.gif"
+  ,url:"/c/programs/demos/hampsterDance/index.html"
+  ,bodyClass: "skin_light skin_inset"
+  ,width:"600"
+  ,height:"400"
+}
+,'<br>',
+{
+   title: "Tunnel"
+  ,ico:"tunnel"
+  ,url:"/c/programs/demos/tunnel/index.html"
+  ,width:"500"
+  ,height:"440"
+}
+,'<br>',
+{
+   title: "TRASH"
+  ,ico:"recycle_bin"
+  ,url:"/c/trash/"
+  ,width:"440"
+  ,height:"330"
+}
+])
+
